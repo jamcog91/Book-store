b990a6d6e6b2247da9877ed610bd5b444dda7410
James Cogan <james.m.cogan@gmail.com>
2023-09-13 21:23:29 -0400

    header layout updates

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,10 +2,12 @@ import './Header.css';
 
 function Header() {
     return(
-        <div className="header__container">
+        <div className='header'>
             <h1>Page Trader</h1>
-            <div>Sign Up</div>
-            <div>Log In</div>
+            <div className='header__nav'>
+                <div className='header__option'>Sign Up</div>
+                <div className='header__option'>Log In</div>
+            </div>
         </div>
     );
 };
